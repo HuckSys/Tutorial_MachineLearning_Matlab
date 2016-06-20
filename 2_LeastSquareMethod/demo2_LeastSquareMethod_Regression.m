ca6f18e72029dc214321bb2f8ce403c3313d4747
Terry Taewoong Um <terry.t.um@gmail.com>
2016-06-20 06:41:13 -0400

    Update demo2_LeastSquareMethod_Regression.m

--- 2_LeastSquareMethod/demo2_LeastSquareMethod_Regression.m
+++ 2_LeastSquareMethod/demo2_LeastSquareMethod_Regression.m
@@ -6,16 +6,11 @@
 
 % Please leave the author information when you reuse the codes.
 
-% [x_train, y_train, x_test, y_test] = GenerateData(opt, bShow, nData, sigma, test_ratio)
-
-% opt : {class_gaussian(defalut), class_half_circle, class_full_circle, class_spiral,
-%        regress_quad, regress_exp, regress_xsinx, regress_exp_xsinx, 
-%        twolink_linear (Terry's Lie group toolbox required, 
-%        [Link] https://github.com/terryum/Human-Robot-Motion-Simulator-based-on-Lie-Group)}
-% bShow : {0: Don't plot, 1(defalut): plot}
-% nData : number of data (defalut: 1000)
-% sigma : noise standard deviation (default: 1)
-% test_ratio : ratio of test data (default: 0.2)
+% [err_train, err_test, loss] = LeastSquareMethod_2D(x_train, y_train, x_test, y_test, nEpoch, stepsize, minibatch) 
+% 
+% nEpoch : number of epochs (default: 200)
+% stepsize : update rate (default: 0.001)
+% minibatch : number of required data for every update (default: 50)
 
 close all;  clearvars;
 
@@ -26,10 +21,10 @@ close all;  clearvars;
 [err_train, err_test, loss] = LeastSquareMethod_2D(x_train, y_train, x_test, y_test);
 
 [x_train, y_train, x_test, y_test] = GenerateData('regress_xsinx', false);
-[err_train, err_test, loss] = LeastSquareMethod_2D(x_train, y_train, x_test, y_test);
+[err_train, err_test, loss] = LeastSquareMethod_2D(x_train, y_train, x_test, y_test, 300);
 
-[x_train, y_train, x_test, y_test] = GenerateData('regress_exp_xsinx', false);
-[err_train, err_test, loss] = LeastSquareMethod_2D(x_train, y_train, x_test, y_test);
+[x_train, y_train, x_test, y_test] = GenerateData('regress_exp_xsinx', false); 
+[err_train, err_test, loss] = LeastSquareMethod_2D(x_train, y_train, x_test, y_test, 400, 0.001, 40);
 
 [x_train, y_train, x_test, y_test] = GenerateData('multi_linear', false, 1200, 0.5);
 [err_train, err_test, loss] = LeastSquareMethod_ND(x_train, y_train, x_test, y_test);
